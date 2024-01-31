c04fdfef04a48989aba66746bb533660ad8495e6
Adina <adinaa_lavinia@yahoo.com>
2024-01-31 13:37:13 +0200

    js

---
+++ scripts/script-detail.js
@@ -0,0 +1,10 @@
+// let modal = document.querySelector(".myModal");
+// let closeBtn = document.getElementsByClassName("close");
+let btn = document.querySelector(".button-box");
+
+btn.addEventListener('click', () => {
+console.log('hello')
+});
+
+// function closeImage() {
+// }
--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 function toggleBtn(){
     let menu = document.querySelector('.menu-box');
-    let button = document.querySelector('.toggle-btn');
-    let dropdown = document.querySelector('.dropdown');
 
     if(menu.style.display == 'block'){
         menu.style.display = 'none';
     } else {
         menu.style.display = 'block';
     }
-
-
-    
-
-
 }
+
+
+
+
+
+
+
