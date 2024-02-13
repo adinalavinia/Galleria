9c5db3a2bfc15c36d47669012306f65cfc7db708
Adina <adinaa_lavinia@yahoo.com>
2024-02-13 13:11:17 +0200

    js

---
+++ scripts/modal-shopping-cart.js
@@ -0,0 +1,14 @@
+const checkoutBtn = document.querySelector('.checkout-button');
+const modalSendOrder = document.querySelector('.modal-send-order');
+const closeMessageSendOrder = document.querySelector('.close-send-order');
+checkoutBtn.addEventListener('click', sendOrder);
+function sendOrder(){
+    modalSendOrder.style.display = 'flex';
+    modalSendOrder.style.alignItems = 'center';
+    modalSendOrder.style.justifyContent = 'center';
+}
+
+closeMessageSendOrder.addEventListener('click', closeModalOrder);
+function closeModalOrder(){
+    modalSendOrder.style.display = 'none';
+}
--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,25 +1,5 @@
-// modal
 
-const checkoutBtn = document.querySelector('.checkout-button');
-const modalSendOrder = document.querySelector('.modal-send-order');
-const closeMessageSendOrder = document.querySelector('.close-send-order');
-checkoutBtn.addEventListener('click', sendOrder);
-function sendOrder(){
-    modalSendOrder.style.display = 'flex';
-    modalSendOrder.style.alignItems = 'center';
-    modalSendOrder.style.justifyContent = 'center';
-}
-
-closeMessageSendOrder.addEventListener('click', closeModalOrder);
-function closeModalOrder(){
-    modalSendOrder.style.display = 'none';
-}
-
-
-
-
-
-// const addToCartBtn = document.querySelector('.shopping-cart');
+const addToCartBtn = document.querySelector('.shopping-cart');
 // const itemsList = document.querySelector('.cart-items');
 
 // addToCartBtn.addEventListener('click', addToShoppingList);
