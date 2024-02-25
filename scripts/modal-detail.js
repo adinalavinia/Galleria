4bb7b56315a512760d4b220247ae69232abf9811
Adina <adinaa_lavinia@yahoo.com>
2024-02-25 12:56:15 +0200

    js

--- scripts/modal-detail.js
+++ scripts/modal-detail.js
@@ -16,13 +16,22 @@ function closeImage() {
     modal.style.display = 'none';
 }
 
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
 
+const shoppingCartBtn = document.querySelector('.shopping-cart');
+const popup = document.querySelector('.popup-text');
+shoppingCartBtn.addEventListener('click', showPopup)
 
-
-
-
-
-
+function showPopup(){
+    popup.style.display = "flex"
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 3000)
+}
 
 
 
